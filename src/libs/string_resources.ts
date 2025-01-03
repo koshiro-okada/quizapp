c2cf5ef471367e4a53c02e87585b3ffc9cb4af6e
Koshiro Okada <koshiro.okada.30@gmail.com>
2025-01-03 11:53:21 +0900

    feat: ヘッダーからサインアウトできるようにする

--- src/libs/string_resources.ts
+++ src/libs/string_resources.ts
@@ -2,6 +2,7 @@ export const stringResources = (() => {
   const resources = {
     systemName: "quizapp",
     signInWithGoogle: "Google アカウントでサインインしてください",
+    signout: "サインアウト",
   } as const;
   return {
     ...resources,
